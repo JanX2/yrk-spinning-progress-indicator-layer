7a810e43d7c7a559635507d1a019057fbcc279f6
Jan Weiß <jan@geheimwerk.de>
2014-11-05 16:23:13 +0100

    Cleanup.

--- Code/YRKSpinningProgressIndicatorLayer.m
+++ Code/YRKSpinningProgressIndicatorLayer.m
@@ -310,7 +310,7 @@
     CGPoint finPosition = CGPointMake([self bounds].size.width/2, [self bounds].size.height/2);
     CGFloat finCornerRadius = finBounds.size.width/2;
 
-    for (NSUInteger i=0; i<_numFins; i++) {
+    for (NSUInteger i = 0; i < _numFins; i++) {
         CALayer *newFin = [CALayer layer];
 
         newFin.bounds = finBounds;
